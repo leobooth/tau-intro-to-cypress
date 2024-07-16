dc244e5ed828d3622917366de717c8cef8461220
Leo Booth <leoboothtx@gmail.com>
2024-07-16 09:45:30 -0500

    completed chapter 6; includes examples for 'Enter' button, mouseover, force-click

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: 'http://localhost:3000',
+    // baseUrl: 'https://todomvc.com/examples/react/dist/',
     // viewportHeight: 550,
     // viewportWidth: 660
+    experimentalStudio: true
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,20 @@
-it('my first test', () => {
-    cy.visit('https://cypress.io')
+function addTodo(todoText) {
+    let todo1 = cy.get("[data-testid='text-input']");
+    todo1.click();
+    todo1.type(todoText);
+    todo1.type("{enter}");
+}
+
+function removeTodo(todoText) {
+    let todoItem = cy.get("[data-testid='todo-item-label']").contains(todoText);
+    todoItem.trigger('mouseover');
+    let todoItemRemoveButton = cy.get("[data-testid='todo-item-button']");
+    todoItemRemoveButton.click({force: true});
+}
+
+it('add and remove a single todo item', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/');
+    addTodo('todo1');
+    removeTodo('todo1');
 });
+
