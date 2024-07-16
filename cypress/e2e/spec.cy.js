7b49bd1d155fbdd2575ee445025752eb73dac26c
Leo Booth <leoboothtx@gmail.com>
2024-07-16 14:15:19 -0500

    completed chapter 7; includes examples of assertions

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,13 +8,26 @@ function addTodo(todoText) {
 function removeTodo(todoText) {
     let todoItem = cy.get("[data-testid='todo-item-label']").contains(todoText);
     todoItem.trigger('mouseover');
-    let todoItemRemoveButton = cy.get("[data-testid='todo-item-button']");
+    let todoItemRemoveButton = 
+        cy.get("[data-testid='todo-item-label']").contains(todoText)
+          .siblings("[data-testid='todo-item-button']");
     todoItemRemoveButton.click({force: true});
 }
 
-it('add and remove a single todo item', () => {
+it('add and remove a single todo item using todo item remove button', () => {
     cy.visit('https://todomvc.com/examples/react/dist/');
     addTodo('todo1');
+    let todo1 = cy.get("[data-testid='todo-item-label']").contains('todo1');
+    todo1.should('be.visible');
     removeTodo('todo1');
 });
 
+it('should show two todo items', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/');
+    addTodo('todo1');
+    addTodo('todo2');
+    
+    cy.get("[data-testid='todo-item-label']").should('have.length', 2);
+    removeTodo('todo1');
+    removeTodo('todo2');
+}); 
